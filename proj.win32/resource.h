d0b53863cd63ad54d0dbeebdeb1450a8fdc7c2d9
suilian <wuzhixue7@gmail.com>
2025-12-09 13:48:32 +0800

    Add files via upload

---
+++ proj.win32/resource.h
@@ -0,0 +1,14 @@
+#define IDS_PROJNAME                100
+#define IDR_TESTJS    100
+
+#define ID_FILE_NEW_WINDOW            32771
+
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE    201
+#define _APS_NEXT_CONTROL_VALUE        1000
+#define _APS_NEXT_SYMED_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE        32775
+#endif
+#endif
